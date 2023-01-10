222bb51ce89fa62b11ba0e0d5e2d7bf0b4eeb5d8
jaseel7582 <jaseelmoh7582@gmail.com>
2023-01-10 12:05:45 +0530

    simple calculator

---
+++ claculator.js
@@ -0,0 +1,15 @@
+ function display(num){
+result.value+=num
+
+}
+function allclear(){
+    result.value=''
+}
+function output(){
+   result.value= eval(result.value)
+
+
+}
+function back(){
+   result.value= result.value.slice(0,-1)
+}
